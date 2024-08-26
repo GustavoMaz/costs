78bca2a1dc1620dc358017584fea20b86d85e1eb
Gustavo Mazurkiewitz <gmazurkiewitz@gmail.com>
2024-08-26 20:08:35 -0300

    Criação e implementação do componente de Loading

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -4,6 +4,7 @@ import { useState, useEffect } from 'react'
 
 import Message from "../layout/Message"
 import Container from "../layout/Container"
+import Loading from "../layout/Loading"
 import LinkButton from "../layout/LinkButton"
 import ProjectCard from '../project/ProjectCard'
 
@@ -11,6 +12,7 @@ import styles from './Projects.module.css'
 
 function Projects() {
   const [projects, setProjects] = useState([]);
+  const [removeLoading, setRemoveLoading] = useState(false);
 
   const location = useLocation();
   let message = '';
@@ -19,18 +21,21 @@ function Projects() {
   }
 
   useEffect(() => {
-    fetch('http://localhost:5000/projects', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-      .then((resp) => resp.json())
-      .then((data) => {
-        setProjects(data)
+    setTimeout(() => {
+      fetch('http://localhost:5000/projects', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
       })
-      .catch((err) => console.log(err)
-      )
+        .then((resp) => resp.json())
+        .then((data) => {
+          setProjects(data)
+          setRemoveLoading(true)
+        })
+        .catch((err) => console.log(err)
+        )
+    }, 300)
   }, []);
 
   return (
@@ -41,16 +46,19 @@ function Projects() {
       </div>
       {message && <Message msg={message} type="success" />}
       <Container customClass="start">
-        {projects.length > 0 && 
-          projects.map((project) => ( 
-            <ProjectCard 
+        {projects.length > 0 &&
+          projects.map((project) => (
+            <ProjectCard
               id={project.id}
-              name={project.name} 
-              category={project.category.name} 
-              budget={project.budget} 
+              name={project.name}
+              category={project.category.name}
+              budget={project.budget}
               key={project.id}
             />
-          )
+          ))}
+        {!removeLoading && <Loading />}
+        {removeLoading && projects.length === 0 && (
+          <p>não há projetos ainda</p>
         )}
       </Container>
     </div>
